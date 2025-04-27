b30152442281f9c7b9b154fd4e7273c8a0299ec7
NoixDeXydre <noixdexydre@mailfence.com>
2025-04-27 12:25:32 +0200

    Amélioration de la fluidité de la caméra

--- Assets/Scripts/Camera/SuiviCamera.cs
+++ Assets/Scripts/Camera/SuiviCamera.cs
@@ -7,21 +7,30 @@ public class SuiviCamera : MonoBehaviour
 {
 
     /// <summary>
+    /// Taux de fluidite de 0 à 1.
+    /// </summary>
+    public float fluiditeCamera;
+
+    /// <summary>
     /// Le GameObject à suivre
     /// </summary>
     public Transform objetSuivi;
 
+    private Vector3 positionFinale;
+
+    void Start()
+    {
+        positionFinale = transform.position;
+    }
+
     /// <summary>
     /// Met à jour la position de la caméra par rapport à l'objet suivi.
     /// </summary>
-    void LateUpdate()
+    void FixedUpdate()
     {
+        positionFinale.x = objetSuivi.position.x;
+        positionFinale.y = objetSuivi.position.y;
 
-        Vector3 positionFinale = objetSuivi.position;
-
-        // On garde le Z de la caméra pour éviter des problèmes de perspective.
-        positionFinale.z = transform.position.z;
-
-        transform.position = positionFinale;
+        transform.position = Vector3.Lerp(transform.position, positionFinale, fluiditeCamera);
     }
 }
