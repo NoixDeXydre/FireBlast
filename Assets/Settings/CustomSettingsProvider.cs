7926384083fdd711b1102496391cfa9f52160162
NoixDeXydre <noixdexydre@mailfence.com>
2025-05-08 17:59:12 +0200

    Ajout de la possibilité de faire des paramètres custom

---
+++ Assets/Settings/CustomSettingsProvider.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Génère le menu custom pour les paramètres.
+/// </summary>
+static class CustomSettingsProvider
+{
+    [SettingsProvider]
+    public static SettingsProvider CreateCustomSettingsProvider()
+    {
+
+        var provider = new SettingsProvider("Project/ParametresFireball", SettingsScope.Project)
+        {
+
+            label = "Paramètres Fireball",
+            guiHandler = (searchContext) =>
+            {
+                var settings = ParametresFireball.GetOrCreateSettings();
+                settings.numeroBuild = EditorGUILayout.IntField("Numéro du build officiel", settings.numeroBuild);
+                settings.nomBuild = EditorGUILayout.TextField("Nom du ou des builds", settings.nomBuild);
+                settings.dateDebutCopyright = EditorGUILayout.IntField("Date de création du jeu", settings.dateDebutCopyright);
+                settings.dateFinCopyright = EditorGUILayout.IntField("Date de subsistance du jeu", settings.dateFinCopyright);
+
+                if (GUI.changed)
+                {
+                    EditorUtility.SetDirty(settings);
+                }
+            },
+
+            keywords = new System.Collections.Generic.HashSet<string>(new[] { "custom", "settings", "mycustom" })
+        };
+
+        return provider;
+    }
+}
